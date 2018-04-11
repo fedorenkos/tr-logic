2c079b1fd0740def85dcb018b037d58dc1897345
Serhiy Fedorenko <fedorenkos084@gmail.com>
2018-04-11 16:51:02 +0300

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	var header = new Headhesive('.navbar');
+
+	$('.push').click(function(){
+
+		$('.left').toggleClass('left--transitioned');
+		$('.right').toggleClass('right--transitioned');
+	});
+		
+
+});
